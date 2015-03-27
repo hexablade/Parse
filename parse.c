69d8bf92a0a9aaec0af57dd28430b7cc79986c86
hexablade <Chavi@Crys.local>
2015-03-27 15:45:16 -0400

    V1
    The Beginning of the Universe

---
+++ parse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "token.h"
+
+
+
+
+
---
+++ token.h
@@ -0,0 +1,31 @@
+#ifndef TOKEN_H_
+#define TOKEN_H_
+#include <stdio.h>
+
+#define STRING_LENGTH 512
+//Token stack structure
+typedef struct Tokenizing 
+{
+    
+    char string[STRING_LENGTH];
+    int location;
+    int column;
+    int row;
+
+}Token;
+
+
+// Initializes the token stack structures
+Token init_token(){
+    
+    Token t;
+
+   t.location = 0;
+   t.column = 0;
+   t.row = 0;
+
+   return t;
+}
+
+#endif
+
